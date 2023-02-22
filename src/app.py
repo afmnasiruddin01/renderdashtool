cac8aab624de90b4418239390cb0c0ceb61930a4
A F M Nasir Uddin <afmnasiruddin01@gmail.com>
2023-02-22 22:30:31 +0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 
 import plotly.express as px
 import dash
+from dash import Dash, html, dcc
 import numpy as np
 import pandas as pd
 np.random.seed(10)
@@ -8,11 +9,15 @@ np.random.seed(10)
 app = dash.Dash(__name__)
 server = app.server
 
-
-# print(df.loc[:, 2013])
 fig = px.scatter(x=[12, 15, 16, 50, 30], y=[1, 5, 6, 10, 8],
                  title="A plotly express figure")
-fig.show()
+
+# print(df.loc[:, 2013])
+app.layout = html.Div([
+              dcc.Graph(figure=fig)
+])
+
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
